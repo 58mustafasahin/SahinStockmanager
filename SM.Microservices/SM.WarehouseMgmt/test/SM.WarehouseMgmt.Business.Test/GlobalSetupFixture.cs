3e707141bab8123f8d9e2397c8075010f2d21192
58mustafasahin <58.mustafasahin@gmail.com>
2024-10-10 15:09:50 +0300

    Warehouse layers added.

---
+++ SM.Microservices/SM.WarehouseMgmt/test/SM.WarehouseMgmt.Business.Test/GlobalSetupFixture.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace SM.Warehouse.Business.Test
+{
+    [SetUpFixture]
+    public class GlobalSetupFixture
+    {
+        [OneTimeSetUp]
+        public void RunBeforeAnyTests()
+        {
+        }
+
+        [OneTimeTearDown]
+        public void RunAfterAnyTests()
+        {
+        }
+    }
+}
